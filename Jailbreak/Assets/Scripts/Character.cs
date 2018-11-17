3bafb1c1f0ec266c11cd431dc80fdb92422fcb7a
Anthony Cittadino <45046711+Tenorizor@users.noreply.github.com>
2018-11-16 19:25:25 -0500

    Added player and character movement
    -Player movement script added
    -Character script added(base movement script for all moving entities)

---
+++ Jailbreak/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed; //how fast character moves
+
+    protected Vector2 direction;
+	
+	// Update is called once per frame
+	protected virtual void Update ()
+    {
+        Move(); //calls move class
+    }
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+    }
+}
---
+++ Jailbreak/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Character {
+
+	// Update is called once per frame
+	protected override void Update ()
+    {
+        GetInput();
+
+        base.Update();
+	}
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+    }
+}
